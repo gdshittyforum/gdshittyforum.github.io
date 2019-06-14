fc4351f7b37b182f91063b57a1dee44bc2d356a7
RedLime <33yang33@naver.com>
2019-06-14 12:45:01 +0900

    .

--- js/list.js
+++ js/list.js
@@ -32,6 +32,7 @@ const d = {
 			"author": "Keleru",
 			"more": "none",
 			"id": 49731309,
+			"pass": "000005",
 			"percentToQualify": 57,
 			"verificationVid": "https://www.youtube.com/watch?v=ETrEuWM18Lk",
 			"key": 0
@@ -56,6 +57,7 @@ const d = {
 			"author": "Keleru",
 			"more": "none",
 			"id": 52494510,
+			"pass": "Free copy",
 			"percentToQualify": 57,
 			"verificationVid": "https://www.youtube.com/watch?v=Iu08clOYt8I",
 			"key": 1
@@ -68,6 +70,7 @@ const d = {
 			"author": "Keleru",
 			"more": "none",
 			"id": 49907278,
+			"pass": "123456",
 			"percentToQualify": 57,
 			"verificationVid": "https://www.youtube.com/watch?v=n-1BTK0cIvA",
 			"key": 2
@@ -80,6 +83,7 @@ const d = {
 			"author": "TheNatanaelGD",
 			"more": "none",
 			"id": 55694591,
+			"pass": "085244",
 			"percentToQualify": 57,
 			"verificationVid": "https://www.youtube.com/watch?v=nl2ZP3n9ggk",
 			"key": 3
@@ -92,6 +96,7 @@ const d = {
 			"author": "Megum",
 			"more": "none",
 			"id": 44568953,
+			"pass": "Not allow copy",
 			"percentToQualify": 57,
 			"verificationVid": "https://www.youtube.com/watch?v=qP0v6diamBM",
 			"key": 4
@@ -104,6 +109,7 @@ const d = {
 			"author": "AcropolisBoy",
 			"more": "none",
 			"id": 55367155,
+			"pass": "001006",
 			"percentToQualify": 57,
 			"verificationVid": "https://www.youtube.com/watch?v=qP0v6diamBM",
 			"key": 5
@@ -116,6 +122,7 @@ const d = {
 			"author": "MiiNi03",
 			"more": "none",
 			"id": 52460725,
+			"pass": "Free copy",
 			"percentToQualify": 57,
 			"verificationVid": "https://www.youtube.com/watch?v=vG0ReCqJT_c",
 			"key": 6
@@ -128,6 +135,7 @@ const d = {
 			"author": "MiiNi03",
 			"more": "none",
 			"id": 54644929,
+			"pass": "Free copy",
 			"percentToQualify": 57,
 			"verificationVid": "https://www.youtube.com/watch?v=f8IPsUcXNYI",
 			"key": 7
@@ -140,6 +148,7 @@ const d = {
 			"author": "ZenroiL and more [oSpace]",
 			"more": "ZenroiL, AlexTheGS, Megum, Smajlik, TheAquaZepyr2",
 			"id": 51744070,
+			"pass": "050119",
 			"percentToQualify": 57,
 			"verificationVid": "https://www.youtube.com/watch?v=ErAgqjsx6ng",
 			"key": 8
@@ -151,7 +160,8 @@ const d = {
 			"name": "Shitty Paroxysm",
 			"author": "Megum",
 			"more": "none",
-			"id": 51744070,
+			"id": 46950888,
+			"pass": "135086",
 			"percentToQualify": 57,
 			"verificationVid": "https://www.youtube.com/watch?v=09tv3JWPkIs",
 			"key": 9
@@ -164,6 +174,7 @@ const d = {
 			"author": "oSpace and more [oSpace]",
 			"more": "none",
 			"id": 54868104,
+			"pass": "050319",
 			"percentToQualify": 57,
 			"verificationVid": "https://www.youtube.com/watch?v=none",
 			"key": 10
@@ -176,6 +187,7 @@ const d = {
 			"author": "Segone",
 			"more": "none",
 			"id": 50911887,
+			"pass": "747747",
 			"percentToQualify": 57,
 			"verificationVid": "https://www.youtube.com/watch?v=LiMjp1OqenI",
 			"key": 11
@@ -188,6 +200,7 @@ const d = {
 			"author": "RealOptagonus",
 			"more": "none",
 			"id": 19865573,
+			"pass": "150516",
 			"percentToQualify": 57,
 			"verificationVid": "https://www.youtube.com/watch?v=u4ZzloFrNWQ",
 			"key": 12
@@ -200,6 +213,7 @@ const d = {
 			"author": "Megum",
 			"more": "none",
 			"id": 46901184,
+			"pass": "097643",
 			"percentToQualify": 57,
 			"verificationVid": "https://www.youtube.com/watch?v=6l2kNZpK9Qg",
 			"key": 13
@@ -212,6 +226,7 @@ const d = {
 			"author": "Megum",
 			"more": "none",
 			"id": 50174445,
+			"pass": "987123",
 			"percentToQualify": 57,
 			"verificationVid": "https://www.youtube.com/watch?v=01V2wiWS69o",
 			"key": 14
@@ -224,6 +239,7 @@ const d = {
 			"author": "Megum",
 			"more": "none",
 			"id": 48249992,
+			"pass": "475869",
 			"percentToQualify": 57,
 			"verificationVid": "https://www.youtube.com/watch?v=GoubaPmG4zE",
 			"key": 15
@@ -236,6 +252,7 @@ const d = {
 			"author": "Segone",
 			"more": "none",
 			"id": 48296479,
+			"pass": "597597",
 			"percentToQualify": 57,
 			"verificationVid": "https://www.youtube.com/watch?v=rwQyyG-ysZ8",
 			"key": 16
@@ -248,6 +265,7 @@ const d = {
 			"author": "Megum",
 			"more": "none",
 			"id": 48073424,
+			"pass": "375209",
 			"percentToQualify": 57,
 			"verificationVid": "https://www.youtube.com/watch?v=cUz5AlXaXxU",
 			"key": 17
@@ -260,6 +278,7 @@ const d = {
 			"author": "Megum and more [Megum]",
 			"more": "Megum, TheAquaZephyr2, Axasion, CorporalPikachu, XanPlayzGamez, Izender, oSpace",
 			"id": 50820782,
+			"pass": "135086",
 			"percentToQualify": 57,
 			"verificationVid": "https://www.youtube.com/watch?v=F7wDHu9oJCQ",
 			"key": 18
@@ -272,9 +291,140 @@ const d = {
 			"author": "Megum and more [Megum]",
 			"more": "Megum, Segone, oSpace, Goddardl, DTDU, xSmajlik and AcropolisBoy",
 			"id": 50619560,
+			"pass": "325698",
 			"percentToQualify": 57,
 			"verificationVid": "https://www.youtube.com/watch?v=F7wDHu9oJCQ",
 			"key": 19
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+			],
+			"name": "Shitty Arctic Lights",
+			"author": "IFrankis",
+			"more": "none",
+			"id": 46494762,
+			"pass": "123456",
+			"percentToQualify": 57,
+			"verificationVid": "https://www.youtube.com/watch?v=9tvwV-cMfiQ",
+			"key": 20
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+			],
+			"name": "Shitty Atmosphere",
+			"author": "Megum",
+			"more": "none",
+			"id": 55501961,
+			"pass": "120519",
+			"percentToQualify": 57,
+			"verificationVid": "https://www.youtube.com/watch?v=wnGLzp9gdTY",
+			"key": 21
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+			],
+			"name": "Shitty Death Corridor",
+			"author": "Acropolisboy and more [Acropolisboy]",
+			"more": "Megum, AcropolisBoy, R4mm1337, oSpace, TheAquaZephyr2 (ShadowNinja), AlexTheGS33, IZender, Axasion",
+			"id": 55501961,
+			"pass": "123666",
+			"percentToQualify": 57,
+			"verificationVid": "https://www.youtube.com/watch?v=Rdb-z8zRRYM",
+			"key": 22
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+			],
+			"name": "Shitty Bausha Vortex",
+			"author": "Megum",
+			"more": "none",
+			"id": 48219651,
+			"pass": "100511",
+			"percentToQualify": 57,
+			"verificationVid": "https://www.youtube.com/watch?v=wl69pSw4vb4",
+			"key": 23
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+			],
+			"name": "Shitty Zodiac",
+			"author": "oSpace and more [oSpace]",
+			"more": "oSpace, Deleitor11, Segone, SkullBytes, ShadowNinja441, AcropolisBoy, Goddardl, Megum",
+			"id": 53858796,
+			"pass": "Free copy",
+			"percentToQualify": 57,
+			"verificationVid": "https://www.youtube.com/watch?v=roX0W3rS6L8",
+			"key": 24
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+			],
+			"name": "Shitty Bloodlust",
+			"author": "Segone",
+			"more": "",
+			"id": 46218296,
+			"pass": "910910",
+			"percentToQualify": 57,
+			"verificationVid": "https://www.youtube.com/watch?v=6zu6AoKYLfU",
+			"key": 25
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+			],
+			"name": "Shitty Ziroikabi",
+			"author": "Megum",
+			"more": "",
+			"id": 47413314,
+			"pass": "159357",
+			"percentToQualify": 57,
+			"verificationVid": "https://www.youtube.com/watch?v=ZEtcNuuW8Ys",
+			"key": 26
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+			],
+			"name": "Shitty Artificial Ascent",
+			"author": "Megum and more [Megum]",
+			"more": "Megum, AlexTheGS33, ShadowNinja441, AcropolisBoy, Goddardl, ShuvonIslam777, iZender, Woris376CZ",
+			"id": 51905026,
+			"pass": "789321",
+			"percentToQualify": 57,
+			"verificationVid": "https://www.youtube.com/watch?v=1R_6CLeZ7NU",
+			"key": 27
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+			],
+			"name": "Shitty Dark flare",
+			"author": "Megum and more [Megum]",
+			"more": "Megum, ItzDevGD, Goddardl, ShadowNinja441, Kevinacho18, Miini03, AcropolisBoy, Keleru",
+			"id": 55205684,
+			"pass": "201920",
+			"percentToQualify": 57,
+			"verificationVid": "https://www.youtube.com/watch?v=R0A0khSiIsw",
+			"key": 28
+		},
+		/*=================================================================================*/
+		{
+			"vids": [
+			],
+			"name": "Shitty Visible Ray",
+			"author": "Megum and more [AlexTheGS33]",
+			"more": "Megum, Segone, AlexTheGS33, Goddardl, SkullBytes, ShadowNinja441, ShuvonIslam777, Zenriol",
+			"id": 55174871,
+			"pass": "042019",
+			"percentToQualify": 57,
+			"verificationVid": "https://www.youtube.com/watch?v=6JYh8vNm-EA",
+			"key": 29
 		},
 		/*=================================================================================*/
 		{
@@ -284,6 +434,7 @@ const d = {
 			"author": "Megum",
 			"more": "none",
 			"id": 52617967,
+			"pass": "Free copy",
 			"percentToQualify": 57,
 			"verificationVid": "https://www.youtube.com/watch?v=-u-ijZwZuz4",
 			"legacyKey": 0
