69e2654a4b21e103813a890ee1c6a3824e9f8b89
RedLime <33yang33@naver.com>
2019-06-18 18:35:44 +0900

    .

--- js/userData.js
+++ js/userData.js
@@ -1,16 +1,68 @@
 function getUserData(user) {
+    var user_data = [];
+  
+    for (var i = 0; i < list.length; i++) {
+        var entry = list[i];
+        for (var a = 0 ; a < entry.vids.length ; a++) {
+            var entry2 = entry.vids[a];
+            var isLoot = false;
+            for (var b = 0 ; b < user_data.length ; b++) {
+                if (user_data[b].name.toUpperCase() == entry2.user.toUpperCase()) {
+                    isLoot = true;
+                }
+            }
+            var p = getUserPoint(i+1, entry2.percent, entry.percentToQualify, entry2.hz);
+            if (isLoot == true) {
+                for (var b = 0 ; b < user_data.length ; b++) {
+                    var user_name = user_data[b].name.toUpperCase(); var data_name = entry2.user.toUpperCase();
+                    if (user_name == data_name) {
+                        user_data[b].point = user_data[b].point + p;
+                        
+                        if (user_data[b].highest == "null" && parseInt(entry2.percent) == 100) {
+                            user_data[b].highest = entry.name; 
+                        }
+                        user_data[b].progress.push({map : entry.name.toString(), progress : entry2.percent.toString(), link : entry2.link, score : roundNumber(p,3)});
+                    }
+                }
+            } else {
+                var map = entry.name.toString();
+                if (parseInt(entry2.percent) != 100) {
+                    map = "null";
+                }
+
+                var prog = [];
+                prog.push({map : entry.name.toString(), progress : entry2.percent.toString(), link : entry2.link, score : roundNumber(p,3)});
+
+                user_data.push({name : entry2.user, highest : map, progress : prog, point : p});
+            }
+        }
+    }
+  
+    var sortingField = "point"
+    user_data.sort(function(a, b) {
+    return b[sortingField] - a[sortingField];
+    });
+  
+    for (var i = 0 ; i < user_data.length ; i++) {
+        user_data[i].point = roundNumber(user_data[i].point, 3);
+    }
+
+    user_data[user].progress.sort(function(a, b) {
+        return b["score"] - a["score"];
+    });
+    
     var progresses = '<ol>'; var clears = 0;
-    for (var i = 0 ; i < rank_data[user].progress.length ; i++) {
-        progresses = progresses + '<li><a href="'+rank_data[user].progress[i].link+'" target="blank_">' + rank_data[user].progress[i].map + ' ' + rank_data[user].progress[i].progress + '% </strong>(UP: '+rank_data[user].progress[i].score+')<strong></a></li>'
-        if (rank_data[user].progress[i].progress == 100) {
+    for (var i = 0 ; i < user_data[user].progress.length ; i++) {
+        progresses = progresses + '<li><a href="'+user_data[user].progress[i].link+'" target="blank_">' + user_data[user].progress[i].map + ' ' + user_data[user].progress[i].progress + '% </strong>(UP: '+user_data[user].progress[i].score+')<strong></a></li>'
+        if (user_data[user].progress[i].progress == 100) {
             clears++;
         }
     }
     progresses = progresses + "</ol>"
     Swal.fire({
-        title : "#"+(user+1)+" : "+rank_data[user].name,
-        html : '<center><strong>점수 : '+rank_data[user].point + '<br>'+
-            '최고 기록 : '+rank_data[user].highest+'<br>'+
+        title : "#"+(user+1)+" : "+user_data[user].name,
+        html : '<center><strong>점수 : '+user_data[user].point + '<br>'+
+            '최고 기록 : '+user_data[user].highest+'<br>'+
             '클리어 기록 : '+clears+'개<br>'+
             '<br>보유 기록 : '+progresses+'<br>'+
             '</strong></center>'
