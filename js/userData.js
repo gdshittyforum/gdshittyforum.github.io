98afdad6ece7fe1ff3e2e11de65750b6ce7f2654
RedLime <33yang33@naver.com>
2019-06-24 15:03:36 +0900

    .

--- js/userData.js
+++ js/userData.js
@@ -61,26 +61,11 @@ function getUserData(user) {
     progresses = progresses + "</ol>"
     Swal.fire({
         title : "#"+(user+1)+" : "+user_data[user].name,
-        html : '<center><strong>점수 : '+user_data[user].point + '<br>'+
-            '최고 기록 : '+user_data[user].highest+'<br>'+
-            '클리어 기록 : '+clears+'개<br>'+
-            '<br>보유 기록 : '+progresses+'<br>'+
+        html : '<center><strong>Score : '+user_data[user].point + '<br>'+
+            'Best Record : '+user_data[user].highest+'<br>'+
+            'Completed Levels : '+clears+'개<br>'+
+            '<br>Record List : '+progresses+'<br>'+
             '</strong></center>'
     });
 
-}
-
-function UP_Info() {
-    Swal.fire({
-        html : ' - 유저 포인트가 뭔가요? <br>' +
-            ' <strong>- 한국포럼에서 도입한 포인트 제도로, 각 유저가<br>' +
-            ' 얼마나 어려운맵을 얼마나 많이 깼는지를 수치상으로 나타낸것입니다.</strong><br><br>' +
-            ' - 어떻게 집계되나요? <br>' +
-            ' <strong>- 포럼에 기록이 등재되면 자동으로 점수가 책정되어<br>' +
-            ' 유저 포인트 랭킹에 적용됩니다. <a href="https://cafe.naver.com/geometrydashpnm/82765" target="blank_">(유저포인트 계산법 link)</a></strong><br><br>' +
-            ' - 어떻게 사용하나요? <br>' +
-            ' <strong>- 누가 포럼데몬을 많이 깼는지 확인도 할 수 있고,<br>' +
-            ' 유저 포인트 랭킹에 올라간 플레이어를 누르면 그 플레이어가\n어떤 데몬을 깼는지 상세하게 볼 수 있습니다.<br>더 자세히 보고 싶다면 각 기록마다 영상 링크가 달려있으니, 해당 기능으로 확인 가능합니다.</strong><br><br>' +
-            '<br>'
-    })
 }
